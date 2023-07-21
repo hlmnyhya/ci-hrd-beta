bf37338e9bc62f41a195a6fe80bf96f7a65d07fa
Hilmanadi Yahya <hlmnyhya99@gmail.com>
2023-07-22 07:06:57 +0800

    fixing testing by jazi4

--- application/views/Riwayat/v_riwayat.php
+++ application/views/Riwayat/v_riwayat.php
@@ -43,9 +43,9 @@
                                         <td><?php echo $r->golongan?></td>
                                         <td><?php echo $r->status?></td>
                                         <td>
-                                            <a href="<?php echo base_url('riwayat/editriwayat/'.$r-> id_riwayat)?>"
+                                            <a href="<?php echo base_url('Riwayat/editriwayat/'.$r-> id_riwayat)?>"
                                                 class="btn icon btn-warning"><i class="fa fa-edit"></i></a>
-                                            <a href="<?php echo base_url('riwayat/deleteriwayat/'.$r-> id_riwayat)?>"
+                                            <a href="<?php echo base_url('Riwayat/deleteriwayat/'.$r-> id_riwayat)?>"
                                                 class="btn icon btn-danger"><i class="fa fa-trash"></i></a>
                                         </td>
                                     </tr>
