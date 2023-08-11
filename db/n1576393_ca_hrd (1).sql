5982618607f4d46cc8501f355e9720c0577dee12
Hilmanadi Yahya <hlmnyhya99@gmail.com>
2023-08-11 23:24:36 +0800

    fixing by ary

--- db/n1576393_ca_hrd (1).sql
+++ db/n1576393_ca_hrd (1).sql
@@ -788,7 +788,7 @@ CREATE TABLE `pelamar` (
 CREATE TABLE `perusahaan` (
   `id_perusahaan` int NOT NULL,
   `perusahaan` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `perusahaan`
